d0e9c90b59bcf0f27503e9d51323dc429764506b
robbiemurray98 <robbiemurray98@yahoo.com>
2024-09-28 12:55:36 -0400

    create calc variables and operate functin

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
+// basic mathematical functions
 function add(a,b) {
     return a + b;
-}
+};
 
 function subtract(a,b) {
      return a - b;
-}
+};
 
 function multiply(a,b) {
     return a * b;
-}
+};
 
 function divide(a,b) {
     return a / b;
-}
+};
+
+
+// create variables for calc operation numbers ex. 3 + 5
+let firstNumber;
+let operator;
+let secondNumber;
+
+// create operate function that takes above variables and calls math functions
+function operate(a, b, c) {
+    a = firstNumber;
+    b = operator;
+    c = secondNumber;
+
+    if (b == '+') {
+        add(a,c);
+    } else if(b == '-') {
+        subtract(a,b);
+    } else if(b == '*') {
+        multiply(a,b);
+    } else if(b == '/') {
+        divide(a,b);
+    }
+};

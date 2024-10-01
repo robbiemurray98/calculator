071941cf0be5a6b05d74305c0a3f9f18a1566e3d
robbiemurray98 <robbiemurray98@yahoo.com>
2024-09-30 21:57:32 -0400

    create functional calulator

--- script.js
+++ script.js
@@ -23,17 +23,128 @@ let secondNumber;
 
 // create operate function that takes above variables and calls math functions
 function operate(a, b, c) {
-    a = firstNumber;
-    b = operator;
-    c = secondNumber;
+    a = +a;
+    c = +c;
 
     if (b == '+') {
-        add(a,c);
+       return add(a,c);
     } else if(b == '-') {
-        subtract(a,b);
+        return subtract(a,c);
     } else if(b == '*') {
-        multiply(a,b);
+       return multiply(a,c);
     } else if(b == '/') {
-        divide(a,b);
+        return divide(a,c);
     }
 };
+
+
+
+
+
+
+
+
+
+
+const btn = document.querySelectorAll('.btn');
+
+
+const char = document.querySelectorAll('.char')
+
+
+
+const num = document.querySelectorAll('.num');
+const display = document.querySelector('.screen');
+
+
+num.forEach(button => {
+
+
+    button.addEventListener('click', () => {
+
+
+        let a = display.textContent;
+
+
+
+        if (display.textContent == '*' || a =='/' || a =='+' || a == '-') {
+
+            display.textContent = button.textContent;
+        } else {
+
+
+
+
+            display.textContent = display.textContent + button.textContent;
+
+        }
+
+
+
+
+    });
+});
+
+
+
+
+
+
+// button is clicked n amount of times 
+// then once a special character is clicked those previous numbers are stored 
+// then the spec character is stored
+// then all numbers after the special char but before the equal sign
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+display.textContent = '0';
+
+
+char.forEach(button => {
+    button.addEventListener('click', () => {
+        firstNumber = +display.textContent;
+
+    });
+});
+
+
+
+
+char.forEach(button => {
+    button.addEventListener('click', () => {
+        display.textContent = button.textContent;
+        operator = display.textContent;
+
+    });
+});
+
+
+const equal = document.querySelector('.equal');
+
+
+
+equal.addEventListener('click', () => {
+    secondNumber = +display.textContent;
+    display.textContent = operate(firstNumber, operator, secondNumber);
+
+})
+
+
+
+
+
+
+
